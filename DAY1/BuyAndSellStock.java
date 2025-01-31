41a319bedc3f7c9afc1db2819ae271fff998dad4
akush1207 <aksh95605@gmail.com>
2025-01-31 09:28:41 +0530

    final commit of buy and sell stock

---
+++ DAY1/BuyAndSellStock.java
@@ -0,0 +1,24 @@
+package DAY1;
+
+import java.util.Scanner;
+public class BuyAndSellStock{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no. of days:");
+        int len=sc.nextInt();
+        int[] prices=new int[len];
+        System.out.print("Enter price of each day:");
+        for (int i = 0; i < len; i++) {
+            prices[i]=sc.nextInt();
+        }
+        System.out.println("Max Profit:"+maxProfit(prices));
+    }
+    public static int maxProfit(int[] prices) {
+        int minPrice=Integer.MAX_VALUE,maxProfit=0;
+        for(int price:prices){
+            minPrice=Math.min(minPrice,price);
+            maxProfit=Math.max(maxProfit,price-minPrice);
+        }
+        return maxProfit;
+    }
+}
