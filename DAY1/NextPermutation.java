f81eb50fbf5b08b8d79c09590732d6388ac8a4af
akush1207 <aksh95605@gmail.com>
2025-02-28 13:37:23 +0530

    Final commit of next permutation

---
+++ DAY1/NextPermutation.java
@@ -0,0 +1,48 @@
+package DAY1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NextPermutation {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter array length:");
+        int len=sc.nextInt();
+        int[] nums=new int[len];
+        System.out.println("Enter array elements:");
+        for(int i=0;i<len;i++){
+            nums[i]=sc.nextInt();
+        }
+        nextPermutation(nums);
+        System.out.println("nums:"+ Arrays.toString(nums));
+    }
+    public static void nextPermutation(int[] nums){
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private static void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
