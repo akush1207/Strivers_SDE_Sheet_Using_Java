1743bf8ce7c860ca5b22408c828ce664bdaaf292
akush1207 <aksh95605@gmail.com>
2025-03-01 10:05:40 +0530

    final commit of add two numbers

---
+++ DAY5/AddTwoNumbers.java
@@ -0,0 +1,38 @@
+package DAY5;
+
+public class AddTwoNumbers {
+    public static Node addTwoNumbers(Node l1, Node l2) {
+        Node ans=new Node(-1);
+        Node curr=ans;
+        int carry=0;
+        while(l1!=null||l2!=null||carry==1) {
+            int sum=0;
+            if(l1!=null) {
+                sum+=l1.value;
+                l1=l1.next;
+            }
+            if(l2!=null) {
+                sum+=l2.value;
+                l2=l2.next;
+            }
+            sum+=carry;
+            carry=sum/10;
+            Node toAdd=new Node(sum%10);
+            curr.next=toAdd;
+            curr=toAdd;
+        }
+        return ans.next;
+    }
+    public static void main(String[] args) {
+        SingleLinkedList list=new SingleLinkedList();
+        Node head1=new Node(1);
+        list.add(head1,2);
+        list.add(head1,4);
+        list.display(head1);
+        Node head2=new Node(1);
+        list.add(head2,3);
+        list.add(head2,4);
+        list.display(head2);
+        list.display(addTwoNumbers(head1,head2));
+    }
+}
