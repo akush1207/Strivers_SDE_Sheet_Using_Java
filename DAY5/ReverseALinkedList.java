05cb1e0c6d0e5a3865500c3679e33dd85c6da5fc
akush1207 <aksh95605@gmail.com>
2025-03-01 09:20:58 +0530

    Updated the code for user readability

--- DAY5/ReverseALinkedList.java
+++ DAY5/ReverseALinkedList.java
@@ -1,38 +1,8 @@
 package DAY5;
 
 public class ReverseALinkedList {
-    static class Node{
-        Node next;
-        int value;
-        Node(int value){
-            this.value=value;
-        }
-    }
-    public Node add(Node head,int value){
-        Node toAdd=new Node(value);
-        if (head != null) {
-            toAdd.next = head;
-        }
-        head=toAdd;
-        return head;
-    }
-    public void display(Node head){
-        if(head==null){
-            System.out.println("Linked List is empty.");
-        }else{
-            System.out.print("Linked List: ");
-            Node curr=head;
-            while (curr!=null){
-                System.out.print(curr.value);
-                if(curr.next!=null){
-                    System.out.print("->");
-                }
-                curr=curr.next;
-            }
-            System.out.println();
-        }
-    }
-    public Node reverse(Node head){
+
+    public static Node reverse(Node head){
         Node curr=head,nextNode=null;
         while (curr!=null){
             Node temp=curr.next;
@@ -44,14 +14,14 @@ public class ReverseALinkedList {
     }
 
     public static void main(String[] args) {
-        ReverseALinkedList list=new ReverseALinkedList();
+        SingleLinkedList list=new SingleLinkedList();
         Node head=new Node(1);
-        head=list.add(head,2);
-        head=list.add(head,3);
-        head=list.add(head,4);
-        head=list.add(head,5);
+        list.add(head,2);
+        list.add(head,3);
+        list.add(head,4);
+        list.add(head,5);
         list.display(head);
-        Node newHead=list.reverse(head);
+        Node newHead=reverse(head);
         list.display(newHead);
     }
 }
