c90d131d39b84b23c59d0733794fc2557546166b
akush1207 <aksh95605@gmail.com>
2025-03-02 07:44:30 +0530

    final submit of palindrome linked list

---
+++ DAY6/PalindromeLinkedList.java
@@ -0,0 +1,43 @@
+package DAY6;
+
+import DAY5.Node;
+import DAY5.SingleLinkedList;
+
+public class PalindromeLinkedList {
+    public static boolean isPalindrome(Node head){
+        Node slow=head,fast=head;
+        while (slow!=null && fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        Node mid=reverse(slow);
+        while (mid!=null){
+            if(mid.value!=head.value){
+                return false;
+            }
+            mid=mid.next;
+            head=head.next;
+        }
+        return true;
+    }
+    public static Node reverse(Node node){
+        Node curr=node,prev=null;
+        while (curr!=null){
+            Node temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
+    public static void main(String[] args) {
+        SingleLinkedList list=new SingleLinkedList();
+        Node head=new Node(1);
+        list.add(head,2);
+        list.add(head,3);
+        list.add(head,8);
+        list.add(head,1);
+        list.display(head);
+        System.out.println("Is Palindrome:"+isPalindrome(head));
+    }
+}
